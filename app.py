559e700f52bd416e2a0a03228f6414c2d8359bd7
EC2 Default User <ec2-user@ip-172-31-20-175.eu-north-1.compute.internal>
2025-06-03 12:03:40 +0000

    Initial commit: added app.py and templates

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import requests
+
+
+
+app = Flask(__name__)
+
+API_KEY = "df935c4f2a261477f519504d4dbea080"  # Replace with your real key
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    weather = None
+    error = None
+    if request.method == 'POST':
+        city = request.form.get('city')
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            weather = {
+                "city": data['name'],
+                "temp": data['main']['temp'],
+                "description": data['weather'][0]['description'].title(),
+                "humidity": data['main']['humidity'],
+                "wind_speed": data['wind']['speed']
+            }
+        else:
+            error = "City not found. Please enter a valid city name."
+    return render_template("index.html", weather=weather, error=error)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
