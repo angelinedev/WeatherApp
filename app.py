da3b8af9be1b297a7c244efa825497e58daf998e
Angel <angelinehephzibahj@gmail.com>
2025-06-08 21:51:23 +0530

    Assets

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import requests
 app = Flask(__name__)
 
 API_KEY = "df935c4f2a261477f519504d4dbea080"
-
 @app.route('/', methods=['GET', 'POST'])
 def home():
     weather = None
@@ -13,6 +12,7 @@ def home():
         city = request.form.get('city')
         url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
         response = requests.get(url)
+        
         if response.status_code == 200:
             data = response.json()
             weather = {
@@ -21,21 +21,10 @@ def home():
                 "description": data['weather'][0]['description'].title(),
                 "humidity": data['main']['humidity'],
                 "wind_speed": data['wind']['speed'],
-                "condition": get_weather_condition(data['weather'][0]['main'])
             }
         else:
             error = "City not found. Please enter a valid city name."
     return render_template("index.html", weather=weather, error=error)
-
-def get_weather_condition(condition):
-    if condition in ['Rain', 'Drizzle']:
-        return 'rain'
-    elif condition == 'Clear':
-        return 'sunny'
-    elif condition in ['Clouds', 'Overcast']:
-        return 'cloudy'
-    # Add more conditions as needed
-    return ''
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
